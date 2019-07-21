47b87efcfe9977ae780797c4ef992aca1ae51849
ahashans <ahashans@gmail.com>
2019-07-21 19:42:42 +0600

    Added JWT token in signup and login process

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const  mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -24,9 +26,24 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         trim:true    
-    }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true,
+
+        }
+    }]
 
 })
+
+userSchema.methods.generateAuthToken = async function(){
+    const user = this
+    const token = jwt.sign({_id:user._id.toString()}, 'thisismynewapp')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
 
 userSchema.statics.findByCredentials = async(email,password) =>{
     const user = await User.findOne({email})
