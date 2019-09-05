4aeb5e101d23e39b88c9da2d253af4f452d37fc8
Ahashan Alam Sojib <ahashan@linkstaff.co.jp>
2019-09-05 12:27:45 +0600

    Added Populate of MessageThread Participant User Info and Updated Message Model

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,58 +1,53 @@
-const express = require('express')
-const Message = require('../models/message')
-const MessageThread = require('../models/message-thread')
-const router = new express.Router()
-const auth = require('../middlewares/auth')
+const express = require('express');
+const Message = require('../models/message');
+const MessageThread = require('../models/message-thread');
+const router = new express.Router();
+const auth = require('../middlewares/auth');
 
 router.post('/messages/:threadId',auth, async (req, res) => {    
     
 });
 
-router.post('/messages/new/:recipient', auth, async (req, res) => {
-    
+router.post('/messages', auth, async (req, res) => {
     try {
-        if(!req.params.recipient){
-            throw Error("Recipient not found!")
+        if(!req.body.threadId){
+            if(!req.body.recipient){
+                throw Error("Recipient not found!");
+            }
+            const messageThread = new MessageThread({
+                participants:[
+                    req.body.recipient,
+                    req.user._id,
+                ]
+            });
+            await messageThread.save();
+            const message = new Message({
+                ...req.body,
+                threadId:messageThread._id,
+                sender:req.user._id
+            });
+            await message.save()
+            return res.status(201).send(message);
+        }else{
+            if(!req.body.threadId){
+                throw Error("Thread Not Found");
+            }
+            const reply = new Message({
+                ...req.body,
+                sender:req.user._id,
+                threadId: req.body.threadId,
+            });
+            await reply.save();
+            return res.send(reply);
         }
-        const messageThread = new MessageThread({
-            participants:[
-                req.params.recipient,
-                req.user._id,
-            ]
-        })
-        await messageThread.save()
-        const message = new Message({
-            ...req.body,
-            threadId:messageThread._id,
-            sender:req.user._id
-        })
-        await message.save()
-        
-        return res.status(201).send(message)
+
     } catch (e) {
-        console.log(e)
+        console.log(e);
         return res.status(500).send()
     }
 });
-router.post("/messages/reply/:threadId",auth, async (req,res)=>{
-    try{
-        if(!req.params.threadId){
-            throw Error("Thread Not Found");
-        }
-        const reply = new Message({
-            ...req.body,
-            sender:req.user._id,
-            threadId: req.params.threadId,
-        });
-        await reply.save();
-        return res.send(reply);
-    }catch (e) {
-        console.log(e)
-        return res.status(500).send();
-    }
-    
-});
-router.get("/messages/inbox", auth, async (req, res)=>{
+
+router.get("/messages", auth, async (req, res)=>{
     try {
         const messageThreads = await MessageThread.find({participants:req.user._id}, "_id" );
         if(messageThreads){
@@ -61,7 +56,8 @@ router.get("/messages/inbox", auth, async (req, res)=>{
                 const message = await Message.find({threadId:messageThread._id})
                                         .sort({createdAt:-1})
                                         .limit(1)
-                                        .populate({path:'sender', select: "email name"});
+                                        .populate({path:'sender', select: "email name"})
+                                        .populate({path:'threadId', populate:{path:'participants', model:'Users'}});
                 inbox.push(message);
             }
             if(!inbox){
@@ -75,9 +71,9 @@ router.get("/messages/inbox", auth, async (req, res)=>{
 });
 router.get("/messages/:threadId", auth, async(req,res)=>{
     try {
-        const messages  = await Message.find({threadId:req.params.threadId}).select("createdAt body sender")
-                                                                            .sort({createdAt:-1})
-                                                                            .populate({path:'sender', select:'name email'});
+        const messages  = await Message.find({threadId:req.params.threadId}).sort({createdAt:-1})
+                                                                            .populate({path:'sender', select:'name email'})
+                                                                            .populate({path:'threadId', populate:{path:'participants', model:'Users'}});
         if(!messages){
             return res.status(404).send("No message found");
         }                                                                            
