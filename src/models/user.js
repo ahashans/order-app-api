c8afc29b5193b0fcbf4cbaf0c2f14aab7a7a1e22
Ahashan Alam Sojib <ahashan@linkstaff.co.jp>
2020-11-07 22:24:44 +0600

    added keepLoggedIn status for generating jwt token

--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,20 @@
-const  mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
+    email: {
+        type: String,
+        required: true,
+        trim: true,
         lowercase: true,
-        unique:true,
+        unique: true,
         dropDups: true,
         validate(value) {
             if (!validator.isEmail(value)) {
@@ -22,86 +22,90 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    password:{
-        type:String,
-        required:true,
-        trim:true    
+    password: {
+        type: String,
+        required: true,
+        trim: true
     },
-    avatar:{
-        type:mongoose.Schema.Types.String,
-        trim:true
+    avatar: {
+        type: mongoose.Schema.Types.String,
+        trim: true
     },
-    tokens:[{
-        token:{
-            type:String,
-            required:true,
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
 
         }
     }]
 
 })
 
-userSchema.virtual('orders',{
+userSchema.virtual('orders', {
     ref: 'Orders',
     localField: '_id',
-    foreignField:'owner'
+    foreignField: 'owner'
 })
-userSchema.virtual('groups',{
-    ref:'Groups',
-    localField:'_id',
-    foreignField:'participants'
+userSchema.virtual('groups', {
+    ref: 'Groups',
+    localField: '_id',
+    foreignField: 'participants'
 })
-userSchema.methods.generateAuthToken = async function(){
+userSchema.methods.generateAuthToken = async function(keepLoggedIn) {
     const user = this
-    const token = jwt.sign({_id:user._id.toString(), name:user.name},
-        'OrderAppBackend',{ expiresIn: 60 * 60 * 24 * 7})
-    user.tokens = user.tokens.concat({token})
+    let expiryProp = { expiresIn: 60 * 60 * 24 * 7 };
+    if (keepLoggedIn) {
+        expiryProp = {}
+    }
+    const token = jwt.sign({ _id: user._id.toString(), name: user.name },
+        'OrderAppBackend', expiryProp)
+    user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
 }
 
-userSchema.methods.toJSON = function(){
+userSchema.methods.toJSON = function() {
     const user = this
     const userObject = user.toObject()
 
     delete userObject.password
     delete userObject.tokens
-    
+
     return userObject
 }
 
-userSchema.statics.findByCredentials = async(email,password) =>{
-    const user = await User.findOne({email})
-    if(!user){
+userSchema.statics.findByCredentials = async(email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
         throw new Error('Invalid Email')
     }
     const isMatch = await bcrypt.compare(password, user.password)
 
-    if(!isMatch){
+    if (!isMatch) {
         throw new Error('Invalid Password')
     }
 
     return user
 }
-userSchema.statics.findByEmail = async (email)=>{
-    const user = await User.findOne({email})
+userSchema.statics.findByEmail = async(email) => {
+    const user = await User.findOne({ email })
     return user;
 }
-userSchema.statics.isDuplicateUser = async(email) =>{
-    const user = await User.findOne({email})
-    if(user){
+userSchema.statics.isDuplicateUser = async(email) => {
+    const user = await User.findOne({ email })
+    if (user) {
         throw new Error('Duplicate Email')
     }
-    return false    
+    return false
 }
 
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function(next) {
     const user = this
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
     next()
 })
 
-const User  = mongoose.model("Users", userSchema)
+const User = mongoose.model("Users", userSchema)
 module.exports = User
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,115 +1,118 @@
-const express = require('express')
-const User = require('../models/user')
-const auth = require('../middlewares/auth')
-const router = new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const auth = require("../middlewares/auth");
+const router = new express.Router();
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-    try {
-        const isDuplicate = await User.isDuplicateUser(user.email);
-        if(!isDuplicate){
-            await user.save()
-            const token = await user.generateAuthToken()
-            res.status(201).send({user, token})
-        }        
-    } catch (e) {
-        res.status(400).send({msg:e.message})
+  try {
+    const isDuplicate = await User.isDuplicateUser(user.email);
+    if (!isDuplicate) {
+      await user.save();
+      const token = await user.generateAuthToken();
+      res.status(201).send({ user, token });
     }
-})
+  } catch (e) {
+    res.status(400).send({ msg: e.message });
+  }
+});
 
-router.post('/users/login', async (req, res) => {
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken(req.body.keepLoggedIn);
+    return res.send({ user, token });
+  } catch (e) {
+    res.status(400).send({ msg: e.message });
+  }
+});
 
-    try {
-        const user = await User.findByCredentials(req.body.email,req.body.password)
-        const token = await user.generateAuthToken()
-        return res.send({user, token})
-    } catch (e) {
-        res.status(400).send({msg:e.message})  
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send({ msg: e.message });
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send({ msg: e.message });
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users) {
+      return res.send(users);
     }
-})
+    console.log(users);
+    return res.status(404).send({ msg: e.message });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send({ msg: e.message });
+  }
+});
 
-router.post('/users/logout', auth, async(req,res)=>{
-    try {
-        req.user.tokens = req.user.tokens.filter((token)=>{
-            return token.token!== req.token
-        })
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send({msg:e.message})
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ msg: "Validation Error" });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    return res.send(req.user);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({ msg: e.message });
+  }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ msg: e.message });
+  }
+});
+router.get("/users/search/:email", auth, async (req, res) => {
+  try {
+    const user = await User.findByEmail(req.params.email);
+    if (!user) {
+      return res.status(404).send({ msg: "User Not Found" });
     }
-})
-
-router.post('/users/logoutAll', auth, async(req,res)=>{
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send({msg:e.message})
-    }
-})
-
-
-router.get('/users/me',auth, async (req, res) => {
-    res.send(req.user)
-})
-
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        if(users){
-            return res.send(users)
-        }
-        console.log(users);
-        return res.status(404).send({msg:e.message})
-    }catch(e){
-        console.log(e);
-        return res.status(500).send({msg:e.message})
-    }
-})
-
-router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({msg:"Validation Error"})
-    }
-
-    try {
-        updates.forEach(update=>req.user[update]=req.body[update])
-        await req.user.save()
-        return res.send(req.user)
-    } catch (e) {
-        console.log(e)
-        res.status(400).send({msg:e.message})
-    }
-})
-
-router.delete('/users/me',auth, async (req, res) => {
-    try {
-        await req.user.remove()
-        res.send(req.user)
-    } catch (e) {
-        console.log(e)
-        res.status(500).send({msg:e.message})
-    }
-})
-router.get('/users/search/:email', auth, async(req,res)=>{
-    try {
-        const user = await User.findByEmail(req.params.email)
-        if(!user){
-            return res.status(404).send({msg:"User Not Found"})
-        }
-        const userObj = user.toObject()
-        delete userObj.password
-        delete userObj.tokens
-        return  res.status(202).send(userObj)
-    } catch (e) {
-        return res.status(500).send({msg:e.message})
-    }
-})
-module.exports = router
+    const userObj = user.toObject();
+    delete userObj.password;
+    delete userObj.tokens;
+    return res.status(202).send(userObj);
+  } catch (e) {
+    return res.status(500).send({ msg: e.message });
+  }
+});
+module.exports = router;
